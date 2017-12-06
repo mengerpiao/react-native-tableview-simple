fdfc6b886b1f8fab22f62e00e29d49f7dea5a5e8
mengerpiao <939426478@qq.com>
2017-12-06 10:12:01 +0800

    Update CustomCell.js

--- components/CustomCell.js
+++ components/CustomCell.js
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-unresolved */
-import React, {
-  PropTypes,
-} from 'react';
+
+import React from 'react';
+
+import PropTypes from 'prop-types';
 
 import {
   StyleSheet,
