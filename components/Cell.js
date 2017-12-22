096d5e8e8f52c8879fee268586b316f7071825a4
mengerpiao <939426478@qq.com>
2017-12-22 13:07:25 +0800

    import React from 'react';  import PropTypes from 'prop-types';
    import React from 'react';
    
    import PropTypes from 'prop-types';

--- components/Cell.js
+++ components/Cell.js
@@ -1,8 +1,7 @@
 /* eslint-disable import/no-unresolved */
-import React, {
-  Component,
-  PropTypes,
-} from 'react';
+import React from 'react';
+
+import PropTypes from 'prop-types';
 
 import {
   NativeModules,
